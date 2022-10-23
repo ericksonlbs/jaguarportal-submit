0377b0c9d1a3e4443b3ba869bc5a0cb5d441e17e
Erickson Lima <ericksonlbs@outlook.com>
2022-10-23 20:06:05 -0300

    Create repo to use in GitHub Action

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM mcr.microsoft.com/dotnet/sdk:6.0
+
+LABEL "com.github.actions.name"="jaguarportal-submit"
+LABEL "com.github.actions.description"="Submit result analysis SBFL to Jaguar Portal."
+LABEL "com.github.actions.icon"="check-square"
+LABEL "com.github.actions.color"="blue"
+
+LABEL "repository"="https://github.com/ericksonlbs/jaguarportal-submit"
+LABEL "homepage"="https://github.com/ericksonlbs"
+LABEL "maintainer"="Erickson Lima"
+
+# Version numbers of used software
+ENV DOTNETCORE_RUNTIME_VERSION=6.0 \
+    JAGUARPORTAL_SUBMIT_DOTNET_TOOL_VERSION=1.0
+
+# Add Microsoft Debian apt-get feed 
+RUN wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb \
+    && dpkg -i packages-microsoft-prod.deb
+
+# Install the .NET Runtime for JaguarPortalSubmit
+# The warning message "delaying package configuration, since apt-utils is not installed" is probably not an actual error, just a warning.
+# We don't need apt-utils, we won't install it. The image seems to work even with the warning.
+RUN apt-get update -y \
+    && apt-get install --no-install-recommends -y apt-transport-https \
+    && apt-get update -y \
+    && apt-get install --no-install-recommends -y aspnetcore-runtime-$DOTNETCORE_RUNTIME_VERSION
+
+# Install SonarScanner .NET global tool
+RUN dotnet tool install dotnet-jaguarportal --tool-path . --version $SONAR_SCANNER_DOTNET_TOOL_VERSION
+
+# Cleanup
+RUN apt-get -q -y autoremove \
+    && apt-get -q clean -y \
+    && rm -rf /var/lib/apt/lists/* /var/cache/apt/archives/*
+
+ADD entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+set -o pipefail
+set -eu
+
+# Check required parameters has a value
+if [ -z "$INPUT_JAGUARPORTALPROJECTKEY" ]; then
+    echo "Input parameter jaguarPortalProjectKey is required"
+    exit 1
+fi
+if [ -z "$INPUT_JAGUARPORTALPROJECTNAME" ]; then
+    echo "Input parameter jaguarPortalProjectName is required"
+    exit 1
+fi
+if [ -z "$JAGUARPORTAL_APIKEY" ]; then
+    echo "Environment parameter JAGUARPORTAL_APIKEY is required"
+    exit 1
+fi
+
+# List Environment variables that's set by Github Action input parameters (defined by user)
+echo "Github Action input parameters"
+echo "INPUT_JAGUARPORTALPROJECTKEY: $INPUT_JAGUARPORTALPROJECTKEY"
+echo "INPUT_JAGUARPORTALPROJECTNAME: $INPUT_JAGUARPORTALPROJECTNAME"
+echo "INPUT_JAGUARPORTALHOSTNAME: $INPUT_JAGUARPORTALHOSTNAME"
+
+# Environment variables that need to be mapped in Github Action 
+#     env:
+#       JAGUARPORTAL_APIKEY: ${{ secrets.JAGUARPORTAL_APIKEY }}
+#
+# JAGUARPORTAL_APIKEY=[your_apikey_from_jaguarportal]
+
+# Environment variables automatically set by Github Actions automatically passed on to the docker container
+#
+# Example pull request
+# GITHUB_REPOSITORY=theowner/therepo
+# GITHUB_EVENT_NAME=pull_request
+# GITHUB_REF=refs/pull/1/merge
+# GITHUB_HEAD_REF=somenewcodewithouttests
+# GITHUB_BASE_REF=master
+#
+# Example normal push
+# GITHUB_REPOSITORY=theowner/therepo
+# GITHUB_EVENT_NAME="push"
+# GITHUB_REF=refs/heads/master
+# GITHUB_HEAD_REF=""
+# GITHUB_BASE_REF=""
+
+# ---------------------------------------------
+# DEBUG: How to run container manually
+# ---------------------------------------------
+# export JAGUARPORTAL_APIKEY="your_token_from_jaguarportal"
+
+# Simulate Github Action input variables  
+# export INPUT_JAGUARPORTALPROJECTKEY="your_projectkey"
+# export INPUT_JAGUARPORTALPROJECTNAME="your_projectname"
+# export INPUT_JAGUARPORTALHOSTNAME="https://jaguarportalcloud.io"
+
+# Simulate Github Action built-in environment variables
+# export GITHUB_REPOSITORY=theowner/therepo
+# export GITHUB_EVENT_NAME="push"
+# export GITHUB_REF=refs/heads/master
+# export GITHUB_HEAD_REF=""
+# export GITHUB_BASE_REF=""
+#
+# Build local Docker image
+# docker build -t jaguarportal-submit .
+# Execute Docker container
+# docker run --name jaguarportal-submit --workdir /github/workspace --rm -e INPUT_JAGUARPORTALPROJECTKEY -e INPUT_JAGUARPORTALPROJECTNAME -e INPUT_JAGUARPORTALHOSTNAME -e JAGUARPORTAL_APIKEY -e GITHUB_EVENT_NAME -e GITHUB_REPOSITORY -e GITHUB_REF -e GITHUB_HEAD_REF -e GITHUB_BASE_REF -v "/var/run/docker.sock":"/var/run/docker.sock" -v $(pwd):"/github/workspace" jaguarportal-submit
+
+#-----------------------------------
+# Send to JaguarPortal
+#-----------------------------------
+jaguarportal_cmd="/jaguarportal submit /k:\"${INPUT_JAGUARPORTALPROJECTKEY}\" /n:\"${INPUT_JAGUARPORTALPROJECTNAME}\" /d:jaguarportal.apikey=\"${JAGUARPORTAL_APIKEY}\" /d:jaguarportal.host.url=\"${INPUT_JAGUARPORTALHOSTNAME}\""
+
+# Check Github environment variable GITHUB_EVENT_NAME to determine if this is a pull request or not. 
+if [[ $GITHUB_EVENT_NAME == 'pull_request' ]]; then
+    # JaguarPortal wants these variables if build is started for a pull request
+    # JaguarPortal parameters: https://github.com/ericksonlbs/JaguarPortal/wiki/API-Documentation/
+    # jaguarportal.pullrequest.key	                Unique identifier of your PR. Must correspond to the key of the PR in GitHub or TFS. E.G.: 5
+    # jaguarportal.pullrequest.branch	            The name of your PR Ex: feature/my-new-feature
+    # jaguarportal.pullrequest.base	                The long-lived branch into which the PR will be merged. Default: master E.G.: master
+    # jaguarportal.pullrequest.github.repository	SLUG of the GitHub Repo (owner/repo)
+
+    # Extract Pull Request numer from the GITHUB_REF variable
+    PR_NUMBER=$(echo "$GITHUB_REF" | awk 'BEGIN { FS = "/" } ; { print $3 }')
+
+    # Add pull request specific parameters
+    jaguarportal_cmd="$jaguarportal_cmd /d:jaguarportal.pullrequest.key=$PR_NUMBER /d:jaguarportal.pullrequest.branch=$GITHUB_HEAD_REF /d:jaguarportal.pullrequest.base=$GITHUB_BASE_REF /d:jaguarportal.pullrequest.github.repository=$GITHUB_REPOSITORY /d:jaguarportal.pullrequest.provider=github"
+
+fi
+
+#-----------------------------------
+# Execute shell commands
+#-----------------------------------
+echo "Shell commands"
+
+#Run command
+echo "jaguarportal_cmd: $jaguarportal_cmd"
+sh -c "$jaguarportal_cmd"
