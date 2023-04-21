429584e80f89d1ad8469e01542860a156a1bf00d
Erickson Lima <ericksonlbs@outlook.com>
2023-04-21 13:55:43 -0300

    fixed file path

--- dotnet-jaguarportal/JaguarPortal/Services/JaguarPortalServices.cs
+++ dotnet-jaguarportal/JaguarPortal/Services/JaguarPortalServices.cs
@@ -76,7 +76,7 @@ namespace dotnet_jaguarportal.JaguarPortal.Services
                     Jaguar2Model lineObj = new Jaguar2Model(line);
 
                     //string notice = $"::notice file={lineObj.FileName},line={lineObj.NumberLine},endLine={lineObj.NumberLine},title=Suspecious-{lineObj.SuspiciousValue}::{JsonSerializer.Serialize(lineObj)}";
-                    string notice = $"::warning file=./{parameters.PathTarget}{lineObj.FullName}.java,line={lineObj.NumberLine},endLine={lineObj.NumberLine},title=Suspecious::Suspecious Text Message";
+                    string notice = $"::warning file=./{parameters.PathTarget}/{lineObj.FullName}.java,line={lineObj.NumberLine},endLine={lineObj.NumberLine},title=Suspecious::Suspecious Text Message2";
                     Console.WriteLine(notice);
 
                     obj.Add(lineObj);
